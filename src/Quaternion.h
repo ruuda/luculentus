721e61330738340892a4c9744ea90ad99c146dc4
Ruud van Asseldonk <dev@veniogames.com>
2014-08-06 19:28:54 +0200

    switch order in Quaternion multiplication for nicer symmetry

--- src/Quaternion.h
+++ src/Quaternion.h
@@ -99,7 +99,7 @@ namespace Luculentus
   {
     Quaternion prod =
     {
-      a.x * b.w  +  a.w * b.x  +  a.y * b.z  -  a.z * b.y,
+      a.w * b.x  +  a.x * b.w  +  a.y * b.z  -  a.z * b.y,
       a.w * b.y  -  a.x * b.z  +  a.y * b.w  +  a.z * b.x,
       a.w * b.z  +  a.x * b.y  -  a.y * b.x  +  a.z * b.w,
       a.w * b.w  -  a.x * b.x  -  a.y * b.y  -  a.z * b.z
